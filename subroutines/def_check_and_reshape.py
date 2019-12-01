16b8e180d7ae1f8e95c8fa5c1fa1d8a2b8d5e704
Finn Heukamp <Finn.Heukamp@t-online.de>
2019-12-01 10:31:05 +0100

    Added Check and Reshape subroutine

--- subroutines/def_check_and_reshape.py
+++ subroutines/def_check_and_reshape.py
@@ -1,12 +1,18 @@
 # Check and reshape Data
 import numpy as np
 
-def check_and_reshape(U: np.ndarray = np.ndarray, V: np.ndarray = np.ndarray) -> np.ndarray:
+def check_and_reshape(U: np.ndarray = np.ndarray,
+                      V: np.ndarray = np.ndarray,
+                      LAT: np.ndarray = np.ndarray) -> np.ndarray:
 
         if not np.shape(U) == np.shape(V):
             print('Data dimensions not consitent: Check dimensions')
         else:
             U_vec = np.concatenate(U)
             V_vec = np.concatenate(V)
+
+            n = len(U_vec) / len(LAT)
+            LAT_vec = np.tile(np.concatenate(LAT),n)
+
 
             return(U_vec, V_vec)
