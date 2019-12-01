528aca05e6e5279a3f8d0b5c670372c501364973
Finn Heukamp <Finn.Heukamp@t-online.de>
2019-12-01 10:07:59 +0100

    Added subroutines

--- mainroutine/Ekman_Transport.py
+++ mainroutine/Ekman_Transport.py
@@ -7,9 +7,16 @@ def Ekman_Transport(U: np.ndarray = np.ndarray,
                     extend_ranges = False,
                     ekman_layer_constant = True) -> np.ndarray
 
+    # 0. Initialize functions
+    %run def_check_and_reshape.py 
+    %run def_bulkformulas_no_ext_range.py
+    %run def_bulkformulas_ext_range.py
+    %run def_windstress.py
+    %run def_ekman_layer_depth.py # <--- not existing yet
+    %run def_ekman_transp.py ??? # <--- not existing yet
+
     # 1. Check and reshape Data
-        # 1.1 Shape of U, V, LAT, LON
-        # 1.2 Make Vektor out of data arrays
+    U_vec, V_vec = def_check_and_reshape(U,V)
 
     # 2. Compute wind stress
         # - NCEP
@@ -18,9 +25,11 @@ def Ekman_Transport(U: np.ndarray = np.ndarray,
         # -	Yelland and Taylor, 1996
         # -	Large and Yeager, 2004
     if not extend_ranges:
-        %run bulkformulas_no_ext_range.py
+        %run bulkformulas_no_ext_range.py #define bulkformulas
+        # run each bulk formula here
     elif extend_ranges:
-        %run bulkformulas_ext_range.py
+        %run bulkformulas_ext_range.py #define bulkformulas
+        # run each bulk formula here
 
     # 3. Compute varying Ekman Layer Depth
     if not ekman_layer_constant:
