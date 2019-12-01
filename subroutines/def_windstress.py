608596b59bcc69361d2864c19a7da176f77fdecf
Finn Heukamp <Finn.Heukamp@t-online.de>
2019-12-01 10:32:14 +0100

    Added windsresss.py subroutine

--- subroutines/def_windstress.py
+++ subroutines/def_windstress.py
@@ -1,1 +1,19 @@
+import numpy as np
+
+def windstress(Cd,U,V)
+# Set parameters
+rho_air = np.empty_like(Cd)
+rho_air.fill(1.2041) #kg/m³
+
+taux = np.empty_like(Cd)
+tauy = np.empty_like(Cd)
+
+U_reshaped = np.empty_like(Cd)
+V_reshaped = np.empty_like(Cd)
+
+U_reshaped
 # Calculate windstress
+taux[:,:,i] = rho_air * Cd * U_reshaped**2
+tauy[:,:,i] = rho_air * Cd * V_reshaped**2
+
+    return(taux, tauy)
