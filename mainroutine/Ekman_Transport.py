46f205098936b649010aada699b737d9a5bf42e3
Finn Heukamp <Finn.Heukamp@t-online.de>
2019-12-01 15:43:05 +0100

    Added additional computation steps into main routine

--- mainroutine/Ekman_Transport.py
+++ mainroutine/Ekman_Transport.py
@@ -7,29 +7,38 @@ def Ekman_Transport(U: np.ndarray = np.ndarray,
                     extend_ranges = False,
                     ekman_layer_constant = True) -> np.ndarray
 
+# INPUT:
+# U:                        [n x m] zonal wind speed
+# V:                        [n x m] merdidional wind speed
+# LAT:                      [n x m] latitude
+# drag_coeff:               0|1|2|3|4|5|6|7
+# extend_ranges:            True | False
+# ekman_layer_constant:     True | False
+#
+#
+
+
     # 0. Initialize functions
-    %run def_check_and_reshape.py 
-    %run def_bulkformulas_no_ext_range.py
-    %run def_bulkformulas_ext_range.py
+    %run def_check_and_reshape.py
+
+    if not extend_ranges:
+        %run def_bulkformulas_no_ext_range.py
+    else:
+        %run def_bulkformulas_ext_range.p
+
+    %run def_drag_coefficients.py
     %run def_windstress.py
     %run def_ekman_layer_depth.py # <--- not existing yet
     %run def_ekman_transp.py ??? # <--- not existing yet
 
     # 1. Check and reshape Data
-    U_vec, V_vec = def_check_and_reshape(U,V)
+    U_vec, V_vec, LAT_vec = check_and_reshape(U,V,LAT) # Reshapes all input data into vectors
 
-    # 2. Compute wind stress
-        # - NCEP
-        # -	Large and Pond, 1981
-        # -	Trenberth et al., 1990
-        # -	Yelland and Taylor, 1996
-        # -	Large and Yeager, 2004
-    if not extend_ranges:
-        %run bulkformulas_no_ext_range.py #define bulkformulas
-        # run each bulk formula here
-    elif extend_ranges:
-        %run bulkformulas_ext_range.py #define bulkformulas
-        # run each bulk formula here
+    # 2. Compute Drag Coefficients
+    Cd_U, Cd_V = drag_coefficients(U=U_vec, V=V_vec)
+
+    # 3. Compute Wind Stress
+    taux, tauy = windstress(U=U_vec, V=V_vec, Cd_U=Cd_U, Cd_V=Cd_V)
 
     # 3. Compute varying Ekman Layer Depth
     if not ekman_layer_constant:
